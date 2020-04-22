ac755464af4ad7c8d3ae4689a758aba85a2da6d5
Leonardo Barbosa <leonardo.barbosa@techniken.com.br>
2020-04-22 20:40:45 -0300

    as

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 	batata 1
 	batata 2
 <<<<<<< HEAD
+<<<<<<< HEAD
 	my egg is very big
 	sdljsd
+=======
+>>>>>>> parent of 96f4962... To falando que meu ovo eh mt grande...
 =======
 >>>>>>> parent of 96f4962... To falando que meu ovo eh mt grande...
 */
