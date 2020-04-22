575ea4fc2106dfb49a5ebfc70fc0fefb5a054414
Leonardo Barbosa <leonardo.barbosa@techniken.com.br>
2020-04-22 20:42:47 -0300

    bosta

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 	batata 2
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	my egg is very big
 	sdljsd
+=======
+>>>>>>> parent of 96f4962... To falando que meu ovo eh mt grande...
 =======
 >>>>>>> parent of 96f4962... To falando que meu ovo eh mt grande...
 =======
